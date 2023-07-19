9d2805d5951bffecdca0cd6d03038c5baefbb5e2
Eduardo <ms7rbeta@gmail.com>
2023-07-18 22:28:23 -0600

    Update run_test.sh

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,2 +1,2 @@
-docker run -d --name siget-test -v siget-test-vol:/siget-vol -p 172.16.4.200:5000:5000 --env DOCKER=1 edo/siget-test:1.0
+docker run -d --name siget-test -v siget-test-vol:/siget-vol -p 172.16.4.200:5000:5000 --env DOCKER=1 --restart always edo/siget-test:1.0
 docker ps -a
