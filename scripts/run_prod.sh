47eae24460162fab1ead0e905471c96ba12db0e5
Eduardo <ms7rbeta@gmail.com>
2023-07-18 22:28:11 -0600

    Update run_prod.sh

--- scripts/run_prod.sh
+++ scripts/run_prod.sh
@@ -1,2 +1,2 @@
-docker run -d --name siget-prod -v siget-prod-vol:/siget-vol -p 172.16.4.200:80:5000 --env DOCKER=1 edo/siget-prod:1.0
+docker run -d --name siget-prod -v siget-prod-vol:/siget-vol -p 172.16.4.200:80:5000 --env DOCKER=1 --restart always edo/siget-prod:1.0
 docker ps -a
